f9bfe90ef048e32389feb07c28660f26a0cc92f2
David Yoon <qqpo12@naver.com>
2025-04-20 20:15:00 +0900

    Use the RedirectAttributes
    * Used the RedirectAttributes to send param with the redirect

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -31,10 +32,13 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String addItem(Item item) {
-        itemRepository.save(item);
+    public String addItem(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
 
-        return "redirect:/basic/items/" + item.getId();
+
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}")
